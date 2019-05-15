f97675c4388973bb65da9f6a99906fb5378b4635
Alex Martell <Alexmartell6@gmail.com>
2019-05-15 10:03:03 -0400

    final commit

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-paper.addEventListener('onmouseover', function() {
-  let paper = getElementsByClassName('paper')
-  paper.style.height = '160px'
-  paper.style.width = '270px'
+window.addEventListener('scroll', function() {
+  let pics = document.getElementsByClassName('small')
+  if(window.scrollY >= 1) {
+    pics[0].classList.remove('hidden')
+    pics[0].classList.add('show')
+  }
+  if(window.scrollY >= 1) {
+    pics[1].classList.remove('hidden')
+    pics[1].classList.add('show')
+  }
+  if(window.scrollY >= 1) {
+    pics[2].classList.remove('hidden')
+    pics[2].classList.add('show')
+  }
 })
